fbc88c3658571251bdfc30e751b2da73dc45756a
ruhulmirza-spec <ruhul.mirza@harnstech.com>
2025-11-16 20:47:10 +0530

    done

---
+++ app/protfolio/page.jsx
@@ -0,0 +1,7 @@
+export default function Protfolio() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+    </div>
+  );
+}
---
+++ app/services/page.jsx
@@ -0,0 +1,7 @@
+export default function Services() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+    </div>
+  );
+}
