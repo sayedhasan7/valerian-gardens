b240dc64c583878fc29e7dcf81ff8c83e462b42a
ruhulmirza-spec <ruhul.mirza@harnstech.com>
2025-11-28 09:59:51 +0530

    video updated

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -6,7 +6,8 @@ export default function HeroSection() {
   const item = {
     title: "Grow Fresh, Live Fresh",
     title2: "Home Gardening Essentials",
-    mobileVideo:"https://res.cloudinary.com/sayed12m/video/upload/v1763787211/flwqsv6s2ecqyym8k50k.mp4",
+    mobileVideo:
+      "https://res.cloudinary.com/sayed12m/video/upload/v1763787211/flwqsv6s2ecqyym8k50k.mp4",
     video:
       "https://res.cloudinary.com/sayed12m/video/upload/v1763749426/wr4oqn5vpbqssecrbzjn.mp4", // ⭐ replace with your video
     desc: "Transform your space with easy home gardening tips. Grow fresh vegetables, herbs, and flowers at home—perfect for beginners.",
@@ -24,15 +25,21 @@ export default function HeroSection() {
           playsInline
         >
           {/* MOBILE VIDEO (max-width: 768px) */}
-          <source src={item.mobileVideo} media="(max-width: 768px)" type="video/mp4" />
-
+          <source
+            src={item.mobileVideo}
+            media="(max-width: 768px)"
+            type="video/mp4"
+          />
           {/* DESKTOP VIDEO */}
-          <source src={item.video} media="(min-width: 769px)" type="video/mp4" />
-
+          {/* <source src={item.video} media="(min-width: 769px)" type="video/mp4" /> */}
+          <source
+            src="../testing.mp4"
+            media="(min-width: 769px)"
+            type="video/mp4"
+          />
           {/* Fallback */}
           Your browser does not support the video tag.
         </video>
-
 
         {/* ⭐ OVERLAY GRADIENT */}
         {/* <div className="absolute inset-0 bg-linear-to-b from-green-900/70 via-green-800/60 to-green-900/70 opacity-95"></div> */}
@@ -46,17 +53,26 @@ export default function HeroSection() {
               </span>
             </div>
 
-            <h1 data-aos="fade-in" className="text-white text-4xl font-signika sm:text-5xl md:text-7xl font-bold leading-tight mb-6">
+            <h1
+              data-aos="fade-in"
+              className="text-white text-4xl font-signika sm:text-5xl md:text-7xl font-bold leading-tight mb-6"
+            >
               {item.title}
               <br />
               {item.title2}
             </h1>
 
-            <p data-aos="fade-in" className="text-white/90 border-t pt-2 font-signika text-lg mb-10 max-w-2xl">
+            <p
+              data-aos="fade-in"
+              className="text-white/90 border-t pt-2 font-signika text-lg mb-10 max-w-2xl"
+            >
               {item.desc}
             </p>
 
-            <Link href={"/contact-us"} className="bg-white w-fit text-gray-900 px-8 py-4 font-semibold hover:bg-gray-100 transition-all flex items-center rounded-full gap-2">
+            <Link
+              href={"/contact-us"}
+              className="bg-white w-fit text-gray-900 px-8 py-4 font-semibold hover:bg-gray-100 transition-all flex items-center rounded-full gap-2"
+            >
               Contact Us
               <svg
                 width="20"
